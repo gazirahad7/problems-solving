17cf17d3fcb5329adbfb2d701289d38b72287405
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-07-17 23:56:04 +0600

    Added: Problem Name: reverse words by length

---
+++ basic/reverse-words-by-length.js
@@ -0,0 +1,18 @@
+// Problem Name: reverse words by length
+const input = "My name is Gazi Rahad";
+// output:
+
+function reversWords(data) {
+  // TODO:  apply logic
+
+  const getWords = data.split(" ");
+
+  // console.log({ getWords });
+
+  const output = getWords.map((word) => {
+    return word.split("").reverse().join(" ");
+  });
+
+  return output;
+}
+console.log(reversWords(input));
